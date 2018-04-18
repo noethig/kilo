7dbea11ffbbbdaf9da9ffb9a88f11cc2169b53b5
Martin Nöthig <elnecesario7@gmail.com>
2018-04-18 22:53:53 +0200

    removed syntax highlighting

--- kiloCrypt.c
+++ kiloCrypt.c
@@ -523,25 +523,6 @@ int editorSyntaxToColor(int hl) {
     }
 }
 
-/* Select the syntax highlight scheme depending on the filename,
- * setting it in the global state E_CONFIG.syntax. */
-void editorSelectSyntaxHighlight(char *filename) {
-    for (unsigned int j = 0; j < HLDB_ENTRIES; j++) {
-        struct editorSyntax *s = HLDB+j;
-        unsigned int i = 0;
-        while(s->filematch[i]) {
-            char *p;
-            int patlen = strlen(s->filematch[i]);
-            if ((p = strstr(filename,s->filematch[i])) != NULL) {
-                if (s->filematch[i][0] != '.' || p[patlen] == '\0') {
-                    E_CONFIG.syntax = s;
-                    return;
-                }
-            }
-            i++;
-        }
-    }
-}
 #pragma endregion "Syntax highlight color scheme"
 
 
@@ -1293,7 +1274,6 @@ int main(int argc, char **argv) {
     }
 
     initEditor();
-    editorSelectSyntaxHighlight(argv[1]);
     editorOpen(argv[1]);
     enableRawMode(STDIN_FILENO);
     editorShowHelpInStatusMessage();
