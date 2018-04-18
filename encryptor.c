3ba1180a0a1196e15da41c0e2d1476b847af2a1d
Martin Nöthig <elnecesario7@gmail.com>
2018-04-18 23:19:59 +0200

    x

--- encryptor.c
+++ encryptor.c
@@ -23,30 +23,32 @@ typedef struct encryptor_struct_t {
 } encryptor_struct_t;
 
 
- 
+void openFile(FILE* fp){
+    char buf[bufSize];
+
+    if ((fp = fopen(fp, "r")) == NULL)
+    { /* Open source file. */
+        perror("fopen source-file");
+        return 1;
+    }
+    
+    while (fgets(buf, sizeof(buf), fp) != NULL)
+    {
+        buf[strlen(buf) - 1] = '\0'; // eat the newline fgets() stores
+        printf("%s\n", buf);
+    }
+    fclose(fp);
+}
  
 int main(int argc, char *argv[])
 {
-  FILE* fp;
-  char buf[bufSize];
   if (argc != 2)
   {
     fprintf(stderr,
             "Usage: %s <soure-file>\n", argv[0]);
     return 1;
   }
-  if ((fp = fopen(argv[1], "r")) == NULL)
-  { /* Open source file. */
-    perror("fopen source-file");
-    return 1;
-  }
- 
-  while (fgets(buf, sizeof(buf), fp) != NULL)
-  {
-    buf[strlen(buf) - 1] = '\0'; // eat the newline fgets() stores
-    printf("%s\n", buf);
-  }
-  fclose(fp);
+  openFile(argv[1]);
   return 0;
 }
 
